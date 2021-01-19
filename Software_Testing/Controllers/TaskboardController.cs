5820652d493573960dba84ecfaf4cd60c8bfe365
Alperen <alperenoz1464@gmail.com>
2021-01-20 01:44:11 +0300

    Add project files.

---
+++ Software_Testing/Controllers/TaskboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Software_Testing.Controllers
+{
+    public class TaskboardController : Controller
+    {
+        public IActionResult Board()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Software_Testing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Software_Testing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
